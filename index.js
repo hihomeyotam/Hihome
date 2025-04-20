639924f4a66a65157ef5465f0ba6c30be76e2165
hihomeyotam <hihomeyotam@gmail.com>
2025-04-20 13:18:27 +0300

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-  return (
-    <div style={{ textAlign: 'center', marginTop: '100px' }}>
-      <h1>ברוכים הבאים ל-HiHome!</h1>
-      <p>אתר נדל"ן חכם וידידותי.</p>
-    </div>
-  );
-}
