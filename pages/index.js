3392007fe8a6075bcfa52a4264ddf5b1fcb8aff5
hihomeyotam <hihomeyotam@gmail.com>
2025-04-20 13:17:28 +0300

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>ברוכים הבאים ל-HiHome!</h1>
+      <p>אתר נדל"ן חכם וידידותי.</p>
+    </div>
+  );
+}
